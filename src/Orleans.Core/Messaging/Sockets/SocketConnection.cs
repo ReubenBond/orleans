74820697a275c2863c8ef8a8805c0cbf746e6ed5
Reuben Bond <rebond@microsoft.com>
2019-05-20 10:53:19 -0700

    Remove dependency on Kestrel internals by copying them in-tree

--- src/Orleans.Core/Messaging/Sockets/SocketConnection.cs
+++ src/Orleans.Core/Messaging/Sockets/SocketConnection.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO.Pipelines;
 using System.Net;
@@ -8,14 +10,428 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Connections;
-using Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.Internal;
+using Microsoft.AspNetCore.Connections.Features;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Logging;
 
 namespace Orleans.Runtime.Messaging
 {
+    public partial class TransportConnection : IFeatureCollection
+    {
+        private static readonly Type IHttpConnectionFeatureType = typeof(IHttpConnectionFeature);
+        private static readonly Type IConnectionIdFeatureType = typeof(IConnectionIdFeature);
+        private static readonly Type IConnectionTransportFeatureType = typeof(IConnectionTransportFeature);
+        private static readonly Type IConnectionItemsFeatureType = typeof(IConnectionItemsFeature);
+        private static readonly Type IMemoryPoolFeatureType = typeof(IMemoryPoolFeature);
+        private static readonly Type IConnectionLifetimeFeatureType = typeof(IConnectionLifetimeFeature);
+        private static readonly Type IConnectionHeartbeatFeatureType = typeof(IConnectionHeartbeatFeature);
+        private static readonly Type IConnectionLifetimeNotificationFeatureType = typeof(IConnectionLifetimeNotificationFeature);
+
+        private object _currentIHttpConnectionFeature;
+        private object _currentIConnectionIdFeature;
+        private object _currentIConnectionTransportFeature;
+        private object _currentIConnectionItemsFeature;
+        private object _currentIMemoryPoolFeature;
+        private object _currentIApplicationTransportFeature;
+        private object _currentITransportSchedulerFeature;
+        private object _currentIConnectionLifetimeFeature;
+        private object _currentIConnectionHeartbeatFeature;
+        private object _currentIConnectionLifetimeNotificationFeature;
+
+        private int _featureRevision;
+
+        private List<KeyValuePair<Type, object>> MaybeExtra;
+
+        private void FastReset()
+        {
+            _currentIHttpConnectionFeature = this;
+            _currentIConnectionIdFeature = this;
+            _currentIConnectionTransportFeature = this;
+            _currentIConnectionItemsFeature = this;
+            _currentIMemoryPoolFeature = this;
+            _currentIApplicationTransportFeature = this;
+            _currentITransportSchedulerFeature = this;
+            _currentIConnectionLifetimeFeature = this;
+            _currentIConnectionHeartbeatFeature = this;
+            _currentIConnectionLifetimeNotificationFeature = this;
+
+        }
+
+        // Internal for testing
+        internal void ResetFeatureCollection()
+        {
+            FastReset();
+            MaybeExtra?.Clear();
+            _featureRevision++;
+        }
+
+        private object ExtraFeatureGet(Type key)
+        {
+            if (MaybeExtra == null)
+            {
+                return null;
+            }
+            for (var i = 0; i < MaybeExtra.Count; i++)
+            {
+                var kv = MaybeExtra[i];
+                if (kv.Key == key)
+                {
+                    return kv.Value;
+                }
+            }
+            return null;
+        }
+
+        private void ExtraFeatureSet(Type key, object value)
+        {
+            if (MaybeExtra == null)
+            {
+                MaybeExtra = new List<KeyValuePair<Type, object>>(2);
+            }
+
+            for (var i = 0; i < MaybeExtra.Count; i++)
+            {
+                if (MaybeExtra[i].Key == key)
+                {
+                    MaybeExtra[i] = new KeyValuePair<Type, object>(key, value);
+                    return;
+                }
+            }
+            MaybeExtra.Add(new KeyValuePair<Type, object>(key, value));
+        }
+
+        bool IFeatureCollection.IsReadOnly => false;
+
+        int IFeatureCollection.Revision => _featureRevision;
+
+        object IFeatureCollection.this[Type key]
+        {
+            get
+            {
+                object feature = null;
+                if (key == IHttpConnectionFeatureType)
+                {
+                    feature = _currentIHttpConnectionFeature;
+                }
+                else if (key == IConnectionIdFeatureType)
+                {
+                    feature = _currentIConnectionIdFeature;
+                }
+                else if (key == IConnectionTransportFeatureType)
+                {
+                    feature = _currentIConnectionTransportFeature;
+                }
+                else if (key == IConnectionItemsFeatureType)
+                {
+                    feature = _currentIConnectionItemsFeature;
+                }
+                else if (key == IMemoryPoolFeatureType)
+                {
+                    feature = _currentIMemoryPoolFeature;
+                }
+                else if (key == IConnectionLifetimeFeatureType)
+                {
+                    feature = _currentIConnectionLifetimeFeature;
+                }
+                else if (key == IConnectionHeartbeatFeatureType)
+                {
+                    feature = _currentIConnectionHeartbeatFeature;
+                }
+                else if (key == IConnectionLifetimeNotificationFeatureType)
+                {
+                    feature = _currentIConnectionLifetimeNotificationFeature;
+                }
+                else if (MaybeExtra != null)
+                {
+                    feature = ExtraFeatureGet(key);
+                }
+
+                return feature;
+            }
+
+            set
+            {
+                _featureRevision++;
+
+                if (key == IHttpConnectionFeatureType)
+                {
+                    _currentIHttpConnectionFeature = value;
+                }
+                else if (key == IConnectionIdFeatureType)
+                {
+                    _currentIConnectionIdFeature = value;
+                }
+                else if (key == IConnectionTransportFeatureType)
+                {
+                    _currentIConnectionTransportFeature = value;
+                }
+                else if (key == IConnectionItemsFeatureType)
+                {
+                    _currentIConnectionItemsFeature = value;
+                }
+                else if (key == IMemoryPoolFeatureType)
+                {
+                    _currentIMemoryPoolFeature = value;
+                }
+                else if (key == IConnectionLifetimeFeatureType)
+                {
+                    _currentIConnectionLifetimeFeature = value;
+                }
+                else if (key == IConnectionHeartbeatFeatureType)
+                {
+                    _currentIConnectionHeartbeatFeature = value;
+                }
+                else if (key == IConnectionLifetimeNotificationFeatureType)
+                {
+                    _currentIConnectionLifetimeNotificationFeature = value;
+                }
+                else
+                {
+                    ExtraFeatureSet(key, value);
+                }
+            }
+        }
+
+        TFeature IFeatureCollection.Get<TFeature>()
+        {
+            TFeature feature = default;
+            if (typeof(TFeature) == typeof(IHttpConnectionFeature))
+            {
+                feature = (TFeature)_currentIHttpConnectionFeature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionIdFeature))
+            {
+                feature = (TFeature)_currentIConnectionIdFeature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionTransportFeature))
+            {
+                feature = (TFeature)_currentIConnectionTransportFeature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionItemsFeature))
+            {
+                feature = (TFeature)_currentIConnectionItemsFeature;
+            }
+            else if (typeof(TFeature) == typeof(IMemoryPoolFeature))
+            {
+                feature = (TFeature)_currentIMemoryPoolFeature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionLifetimeFeature))
+            {
+                feature = (TFeature)_currentIConnectionLifetimeFeature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionHeartbeatFeature))
+            {
+                feature = (TFeature)_currentIConnectionHeartbeatFeature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionLifetimeNotificationFeature))
+            {
+                feature = (TFeature)_currentIConnectionLifetimeNotificationFeature;
+            }
+            else if (MaybeExtra != null)
+            {
+                feature = (TFeature)(ExtraFeatureGet(typeof(TFeature)));
+            }
+
+            return feature;
+        }
+
+        void IFeatureCollection.Set<TFeature>(TFeature feature)
+        {
+            _featureRevision++;
+            if (typeof(TFeature) == typeof(IHttpConnectionFeature))
+            {
+                _currentIHttpConnectionFeature = feature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionIdFeature))
+            {
+                _currentIConnectionIdFeature = feature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionTransportFeature))
+            {
+                _currentIConnectionTransportFeature = feature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionItemsFeature))
+            {
+                _currentIConnectionItemsFeature = feature;
+            }
+            else if (typeof(TFeature) == typeof(IMemoryPoolFeature))
+            {
+                _currentIMemoryPoolFeature = feature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionLifetimeFeature))
+            {
+                _currentIConnectionLifetimeFeature = feature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionHeartbeatFeature))
+            {
+                _currentIConnectionHeartbeatFeature = feature;
+            }
+            else if (typeof(TFeature) == typeof(IConnectionLifetimeNotificationFeature))
+            {
+                _currentIConnectionLifetimeNotificationFeature = feature;
+            }
+            else
+            {
+                ExtraFeatureSet(typeof(TFeature), feature);
+            }
+        }
+
+        private IEnumerable<KeyValuePair<Type, object>> FastEnumerable()
+        {
+            if (_currentIHttpConnectionFeature != null)
+            {
+                yield return new KeyValuePair<Type, object>(IHttpConnectionFeatureType, _currentIHttpConnectionFeature);
+            }
+            if (_currentIConnectionIdFeature != null)
+            {
+                yield return new KeyValuePair<Type, object>(IConnectionIdFeatureType, _currentIConnectionIdFeature);
+            }
+            if (_currentIConnectionTransportFeature != null)
+            {
+                yield return new KeyValuePair<Type, object>(IConnectionTransportFeatureType, _currentIConnectionTransportFeature);
+            }
+            if (_currentIConnectionItemsFeature != null)
+            {
+                yield return new KeyValuePair<Type, object>(IConnectionItemsFeatureType, _currentIConnectionItemsFeature);
+            }
+            if (_currentIMemoryPoolFeature != null)
+            {
+                yield return new KeyValuePair<Type, object>(IMemoryPoolFeatureType, _currentIMemoryPoolFeature);
+            }
+            if (_currentIConnectionLifetimeFeature != null)
+            {
+                yield return new KeyValuePair<Type, object>(IConnectionLifetimeFeatureType, _currentIConnectionLifetimeFeature);
+            }
+            if (_currentIConnectionHeartbeatFeature != null)
+            {
+                yield return new KeyValuePair<Type, object>(IConnectionHeartbeatFeatureType, _currentIConnectionHeartbeatFeature);
+            }
+            if (_currentIConnectionLifetimeNotificationFeature != null)
+            {
+                yield return new KeyValuePair<Type, object>(IConnectionLifetimeNotificationFeatureType, _currentIConnectionLifetimeNotificationFeature);
+            }
+
+            if (MaybeExtra != null)
+            {
+                foreach (var item in MaybeExtra)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        IEnumerator<KeyValuePair<Type, object>> IEnumerable<KeyValuePair<Type, object>>.GetEnumerator() => FastEnumerable().GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => FastEnumerable().GetEnumerator();
+    }
+
+    public abstract partial class TransportConnection : ConnectionContext
+    {
+        private IDictionary<object, object> _items;
+        private List<(Action<object> handler, object state)> _heartbeatHandlers;
+        private readonly object _heartbeatLock = new object();
+        protected readonly CancellationTokenSource _connectionClosingCts = new CancellationTokenSource();
+
+        public TransportConnection()
+        {
+            FastReset();
+
+            ConnectionClosedRequested = _connectionClosingCts.Token;
+        }
+
+        public IPAddress RemoteAddress { get; set; }
+        public int RemotePort { get; set; }
+        public IPAddress LocalAddress { get; set; }
+        public int LocalPort { get; set; }
+
+        public override string ConnectionId { get; set; }
+
+        public override IFeatureCollection Features => this;
+
+        public virtual MemoryPool<byte> MemoryPool { get; }
+        public virtual PipeScheduler InputWriterScheduler { get; }
+        public virtual PipeScheduler OutputReaderScheduler { get; }
+
+        public override IDuplexPipe Transport { get; set; }
+        public IDuplexPipe Application { get; set; }
+
+        public override IDictionary<object, object> Items
+        {
+            get
+            {
+                // Lazily allocate connection metadata
+                return _items ?? (_items = new ConnectionItems());
+            }
+            set
+            {
+                _items = value;
+            }
+        }
+
+        public PipeWriter Input => Application.Output;
+        public PipeReader Output => Application.Input;
+
+        public CancellationToken ConnectionClosed { get; set; }
+
+        public CancellationToken ConnectionClosedRequested { get; set; }
+
+        public void TickHeartbeat()
+        {
+            lock (_heartbeatLock)
+            {
+                if (_heartbeatHandlers == null)
+                {
+                    return;
+                }
+
+                foreach (var (handler, state) in _heartbeatHandlers)
+                {
+                    handler(state);
+                }
+            }
+        }
+
+        public void OnHeartbeat(Action<object> action, object state)
+        {
+            lock (_heartbeatLock)
+            {
+                if (_heartbeatHandlers == null)
+                {
+                    _heartbeatHandlers = new List<(Action<object> handler, object state)>();
+                }
+
+                _heartbeatHandlers.Add((action, state));
+            }
+        }
+
+        // DO NOT remove this override to ConnectionContext.Abort. Doing so would cause
+        // any TransportConnection that does not override Abort or calls base.Abort
+        // to stack overflow when IConnectionLifetimeFeature.Abort() is called.
+        // That said, all derived types should override this method should override
+        // this implementation of Abort because canceling pending output reads is not
+        // sufficient to abort the connection if there is backpressure.
+        public override void Abort(ConnectionAbortedException abortReason)
+        {
+            Output.CancelPendingRead();
+        }
+
+        public void RequestClose()
+        {
+            try
+            {
+                _connectionClosingCts.Cancel();
+            }
+            catch (ObjectDisposedException)
+            {
+                // There's a race where the token could be disposed
+                // swallow the exception and no-op
+            }
+        }
+    }
+
     internal sealed class SocketConnection : TransportConnection, IDisposable
     {
-        private static readonly int MinAllocBufferSize = KestrelMemoryPool.MinimumSegmentSize / 2;
+        private static readonly int MinAllocBufferSize = 2048;
         private static readonly bool IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
         private static readonly bool IsMacOS = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
 
