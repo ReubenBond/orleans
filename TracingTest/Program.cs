b2e71a9ee5c5b308c3495e255643ee4097e6935f
Reuben Bond <rebond@microsoft.com>
2019-10-30 08:45:21 -0700

    WIP test

---
+++ TracingTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TracingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
