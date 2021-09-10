b235c3f3dddb0dbd24f2bc0d65d3b2ba7cce4921
ReubenBond <rebond@microsoft.com>
2021-09-10 05:23:24 -0700

    IOQueue: Use SingleWaiterAutoResetEvent instead of directly enqueuing to ThreadPool for perf (needs verification of perf)

--- src/Orleans.Core/Networking/Shared/IOQueue.cs
+++ src/Orleans.Core/Networking/Shared/IOQueue.cs
@@ -1,63 +1,38 @@
 using System;
 using System.Collections.Concurrent;
 using System.IO.Pipelines;
-using System.Threading;
+using System.Threading.Tasks;
+using Orleans.Runtime;
 
 namespace Orleans.Networking.Shared
 {
-    internal sealed class IOQueue : PipeScheduler,  IThreadPoolWorkItem
+    internal sealed class IOQueue : PipeScheduler
     {
+        private readonly Task _runTask;
         private readonly ConcurrentQueue<Work> _workItems = new();
-        private int _doingWork;
+        private readonly SingleWaiterAutoResetEvent _workSignal = new() { RunContinuationsAsynchronously = true };
+
+        public IOQueue()
+        {
+            _runTask = Task.Run(Run);
+        }
 
         public override void Schedule(Action<object> action, object state)
         {
-            var work = new Work(action, state);
-
-            _workItems.Enqueue(work);
-
-            // Set working if it wasn't (via atomic Interlocked).
-            if (Interlocked.CompareExchange(ref _doingWork, 1, 0) == 0)
-            {
-                // Wasn't working, schedule.
-                _ = System.Threading.ThreadPool.UnsafeQueueUserWorkItem(this, preferLocal: false);
-            }
+            _workItems.Enqueue(new Work(action, state));
+            _workSignal.Signal();
         }
 
-        public void Execute()
+        private async Task Run()
         {
             while (true)
             {
+                await _workSignal.WaitAsync();
+
                 while (_workItems.TryDequeue(out var item))
                 {
                     item.Callback(item.State);
                 }
-
-                // All work done.
-
-                // Set _doingWork (0 == false) prior to checking IsEmpty to catch any missed work in interim.
-                // This doesn't need to be volatile due to the following barrier (i.e. it is volatile).
-                _doingWork = 0;
-
-                // Ensure _doingWork is written before IsEmpty is read.
-                // As they are two different memory locations, we insert a barrier to guarantee ordering.
-                Thread.MemoryBarrier();
-
-                // Check if there is work to do
-                if (_workItems.IsEmpty)
-                {
-                    // Nothing to do, exit.
-                    break;
-                }
-
-                // Is work, can we set it as active again (via atomic Interlocked), prior to scheduling?
-                if (Interlocked.Exchange(ref _doingWork, 1) == 1)
-                {
-                    // Execute has been rescheduled already, exit.
-                    break;
-                }
-
-                // Is work, wasn't already scheduled so continue loop.
             }
         }
 
