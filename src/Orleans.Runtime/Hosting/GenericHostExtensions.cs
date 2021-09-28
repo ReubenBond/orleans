4d8d6c5eaa31cbfef2d641f52953a574037d5e6f
ReubenBond <rebond@microsoft.com>
2021-09-28 07:30:05 -0700

    WIP - fix Minimal APIs support

--- src/Orleans.Runtime/Hosting/GenericHostExtensions.cs
+++ src/Orleans.Runtime/Hosting/GenericHostExtensions.cs
@@ -30,17 +30,13 @@ namespace Microsoft.Extensions.Hosting
             {
                 siloBuilder = new SiloBuilder(hostBuilder);
                 hostBuilder.Properties.Add(siloBuilderKey, siloBuilder);
-                hostBuilder.ConfigureServices((context, services) =>
-                {
-                    siloBuilder.Build(context, services);
-                });
             }
             else
             {
                 siloBuilder = (SiloBuilder)hostBuilder.Properties[siloBuilderKey];
             }
 
-            siloBuilder.ConfigureSilo(configureDelegate);
+            siloBuilder.ConfigureServices((ctx, services) => configureDelegate(ctx, siloBuilder));
             return hostBuilder;
         }
 
