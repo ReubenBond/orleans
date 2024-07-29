2cb3a467e2138000eafb62b76b4f5ba0ffea2f3b
Benjamin Petit <bpetit@microsoft.com>
2024-07-29 15:12:51 +0200

    Another try

--- test/Extensions/TesterAzureUtils/AzureStorageOperationOptionsExtensions.cs
+++ test/Extensions/TesterAzureUtils/AzureStorageOperationOptionsExtensions.cs
@@ -7,8 +7,6 @@ namespace Tester.AzureUtils
 {
     public static class AzureStorageOperationOptionsExtensions
     {
-        public static DefaultAzureCredential Credential = new DefaultAzureCredential();
-
         public static Orleans.Clustering.AzureStorage.AzureStorageOperationOptions ConfigureTestDefaults(this Orleans.Clustering.AzureStorage.AzureStorageOperationOptions options)
         {
             options.TableServiceClient = GetTableServiceClient();
@@ -19,7 +17,7 @@ namespace Tester.AzureUtils
         public static TableServiceClient GetTableServiceClient()
         {
             return TestDefaultConfiguration.UseAadAuthentication
-                ? new(TestDefaultConfiguration.TableEndpoint, Credential)
+                ? new(TestDefaultConfiguration.TableEndpoint, TestDefaultConfiguration.TokenCredential)
                 : new(TestDefaultConfiguration.DataConnectionString);
         }
 
@@ -48,7 +46,7 @@ namespace Tester.AzureUtils
         {
             if (TestDefaultConfiguration.UseAadAuthentication)
             {
-                options.BlobServiceClient = new(TestDefaultConfiguration.DataBlobUri, Credential);
+                options.BlobServiceClient = new(TestDefaultConfiguration.DataBlobUri, TestDefaultConfiguration.TokenCredential);
             }
             else
             {
@@ -62,7 +60,7 @@ namespace Tester.AzureUtils
         {
             if (TestDefaultConfiguration.UseAadAuthentication)
             {
-                options.QueueServiceClient = new(TestDefaultConfiguration.DataQueueUri, Credential);
+                options.QueueServiceClient = new(TestDefaultConfiguration.DataQueueUri, TestDefaultConfiguration.TokenCredential);
             }
             else
             {
@@ -76,7 +74,7 @@ namespace Tester.AzureUtils
         {
             if (TestDefaultConfiguration.UseAadAuthentication)
             {
-                options.BlobServiceClient = new(TestDefaultConfiguration.DataBlobUri, Credential);
+                options.BlobServiceClient = new(TestDefaultConfiguration.DataBlobUri, TestDefaultConfiguration.TokenCredential);
             }
             else
             {
