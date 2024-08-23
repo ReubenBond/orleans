ed7c565cd09398992172a5407e5fd6913c2c8726
Reuben Bond <reuben.bond@gmail.com>
2024-08-22 17:59:27 -0700

    Opt-in to distributed grain directory for tests

--- src/Orleans.Runtime/Hosting/CoreHostingExtensions.cs
+++ src/Orleans.Runtime/Hosting/CoreHostingExtensions.cs
@@ -1,6 +1,7 @@
 #nullable enable
 using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -144,6 +145,13 @@ namespace Orleans.Hosting
             });
         }
 
+        /// <summary>
+        /// Opts-in to the experimental distributed grain directory.
+        /// </summary>
+        /// <param name="siloBuilder">The silo builder to register the directory implementation with.</param>
+        /// <param name="name">The name of the directory to register, or null to register the directory as the default.</param>
+        /// <returns>The provided silo builder.</returns>
+        [Experimental("ORLEANSEXP002")]
         public static ISiloBuilder AddDistributedGrainDirectory(this ISiloBuilder siloBuilder, string? name = null)
         {
             var services = siloBuilder.Services;
