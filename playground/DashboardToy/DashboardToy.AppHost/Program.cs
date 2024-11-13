c9546ac02dc6b48b97042f7b5ff09ba1c195ea41
Reuben Bond <reuben.bond@gmail.com>
2024-11-13 15:43:13 -0800

    Use Aspire now that there are no workloads required

--- playground/DashboardToy/DashboardToy.AppHost/Program.cs
+++ playground/DashboardToy/DashboardToy.AppHost/Program.cs
@@ -6,7 +6,6 @@ var redis = builder.AddRedis("orleans-redis");
 var orleans = builder.AddOrleans("cluster")
     .WithClustering(redis);
 
-// Comment this out once Aspire no longer requires a 'workload' to build.
 builder.AddProject<DashboardToy_Frontend>("frontend")
     .WithReference(orleans)
     .WithReplicas(5);
