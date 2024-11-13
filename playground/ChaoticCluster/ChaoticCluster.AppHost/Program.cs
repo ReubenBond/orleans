d5e4649250b4a3b3afaeaba7085d5cc5f5ac7f5c
Reuben Bond <reuben.bond@gmail.com>
2024-11-13 15:39:29 -0800

    Use Aspire now that there are no workloads required

--- playground/ChaoticCluster/ChaoticCluster.AppHost/Program.cs
+++ playground/ChaoticCluster/ChaoticCluster.AppHost/Program.cs
@@ -2,9 +2,7 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-/*
 // Comment this out once Aspire no longer requires a 'workload' to build.
 builder.AddProject<ChaoticCluster_Silo>("silo");
-*/
 
 builder.Build().Run();
--- playground/DashboardToy/DashboardToy.AppHost/Program.cs
+++ playground/DashboardToy/DashboardToy.AppHost/Program.cs
@@ -6,11 +6,9 @@ var redis = builder.AddRedis("orleans-redis");
 var orleans = builder.AddOrleans("cluster")
     .WithClustering(redis);
 
-/*
 // Comment this out once Aspire no longer requires a 'workload' to build.
 builder.AddProject<DashboardToy_Frontend>("frontend")
     .WithReference(orleans)
     .WithReplicas(5);
-*/
 
 builder.Build().Run();
