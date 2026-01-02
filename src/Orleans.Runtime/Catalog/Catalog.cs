9083c7483470babde9150df1dba98e5b2e7d92f0
Reuben Bond <reuben.bond@gmail.com>
2026-01-01 16:30:10 -0800

    Implement IRemoteGrainDirectory via DistributedGrainDirectory

--- src/Orleans.Runtime/Catalog/Catalog.cs
+++ src/Orleans.Runtime/Catalog/Catalog.cs
@@ -330,7 +330,7 @@ namespace Orleans.Runtime
                         var activationData = activation.Value;
                         var placementStrategy = activationData.GetComponent<PlacementStrategy>();
                         var isUsingGrainDirectory = placementStrategy is { IsUsingGrainDirectory: true };
-                        if (!isUsingGrainDirectory || !grainDirectoryResolver.IsUsingDefaultDirectory(activationData.GrainId.Type)) continue;
+                        if (!isUsingGrainDirectory || !grainDirectoryResolver.IsUsingDhtGrainDirectory(activationData.GrainId.Type)) continue;
                         if (!updatedSilo.Equals(directory.GetPrimaryForGrain(activationData.GrainId))) continue;
 
                         activationsToShutdown.Add(activationData);
